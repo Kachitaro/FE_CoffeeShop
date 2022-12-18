1ce42609e8dc66edb9c610ea76e00daed9de5a01
Kachitaro <anhtai741981@gmail.com>
2022-12-18 10:04:46 +0700

    route

--- src/routes/System.js
+++ src/routes/System.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { Redirect, Route, Switch } from 'react-router-dom';
 import UserManage from '../containers/System/UserManage';
-import ProductManage from '../containers/System/ProductManage';
+import AdminManage from '../containers/System/Admin/ManageAdmin';
 import Header from '../containers/Header/Header';
 class System extends Component {
     render() {
@@ -14,8 +14,8 @@ class System extends Component {
                 <div className="system-container">
                     <div className="system-list">
                         <Switch>
-                            <Route path="/system/user-manage" component={UserManage} />
-                            <Route path="/system/product-manage" component={ProductManage} />ẻ
+                            <Route path="/system/user-staff" component={UserManage} />
+                            <Route path="/system/user-admin" component={AdminManage} />
                             <Route component={() => { return (<Redirect to={systemMenuPath} />) }} />
                         </Switch>
                     </div>
